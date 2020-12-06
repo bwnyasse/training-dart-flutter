c66014b3bbb411f98a7f2c99530d8fae8d3b4deb
bwnyasse <risbonyasse@gmail.com>
2020-12-06 15:23:49 -0500

    core(w_three): platform client TODO

--- workshop_three/lib/main.dart
+++ workshop_three/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Battery Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Battery Demo'),
     );
   }
 }
@@ -31,39 +32,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // Platform
-  static const platform = const MethodChannel('samples.flutter.dev/battery');
+  // Platform The client and host sides of a channel are connected
+  // through a channel name passed in the channel constructor
+  //TODO: Récuperer le channel
 
   // Get battery level.
-  String _batteryLevel = 'Unknown battery level.';
+  String _batteryLevelMsg = 'Unknown battery level.';
+  double _percent = 0;
 
   Future<void> _getBatteryLevel() async {
-    String batteryLevel;
-    try {
-      final int result = await platform.invokeMethod('getBatteryLevel');
-      batteryLevel = 'Battery level at $result % .';
-    } on PlatformException catch (e) {
-      batteryLevel = "Failed to get battery level: '${e.message}'.";
-    }
-
-    setState(() {
-      _batteryLevel = batteryLevel;
-    });
+    //TODO: Méthode qui va récupérer le niveau de la batterie en faisant appel via la méthode 'invokeMethod'
   }
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              child: Text('Get Battery Level'),
-              onPressed: _getBatteryLevel,
-            ),
-            Text(_batteryLevel),
-          ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Workshop N°3 - Platform channel"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              new CircularPercentIndicator(
+                radius: 200.0,
+                lineWidth: 10.0,
+                percent: _percent,
+                center: new Text(_batteryLevelMsg),
+                progressColor: _percent > 0.5 ? Colors.green : Colors.red,
+              ),
+              ElevatedButton(
+                child: Text('Get Battery Level'),
+                onPressed: _getBatteryLevel,
+              ),
+            ],
+          ),
         ),
       ),
     );
