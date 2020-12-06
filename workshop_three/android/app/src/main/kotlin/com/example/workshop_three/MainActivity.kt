e2b05fe93ffa8d1efe16220ad399195474012af2
bwnyasse <risbonyasse@gmail.com>
2020-12-06 15:37:12 -0500

    core(w_three): android specific TODO

--- workshop_three/android/app/src/main/kotlin/com/example/workshop_three/MainActivity.kt
+++ workshop_three/android/app/src/main/kotlin/com/example/workshop_three/MainActivity.kt
@@ -1,6 +1,25 @@
 package com.example.workshop_three
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+
+    //TODO-2: Nommage du channel de communication host <-> client
+
+    //TODO-3: Configuration du framework pour communiquer et récuperer le niveau 
+    // de la batterie via le channel
+
+    //TODO-1: implémentation android de la récupération de la batterie
+
 }
