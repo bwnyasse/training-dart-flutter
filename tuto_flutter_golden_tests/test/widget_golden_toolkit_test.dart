95253cb3d56a6f5580bcd45c500963cfa804db56
bwnyasse <risbonyasse@gmail.com>
2020-12-06 13:43:52 -0500

    core(golden_tests): DeviceBuilder - multiple scenarios - with onCreate

--- tuto_flutter_golden_tests/test/widget_golden_toolkit_test.dart
+++ tuto_flutter_golden_tests/test/widget_golden_toolkit_test.dart
@@ -4,9 +4,7 @@ import 'package:golden_toolkit/golden_toolkit.dart';
 
 import 'package:tuto_flutter_golden_tests/main.dart';
 
-
 void main() {
-
   testGoldens('DeviceBuilder - one scenario - default devices', (tester) async {
     final builder = DeviceBuilder()
       ..addScenario(
@@ -19,7 +17,7 @@ void main() {
     await screenMatchesGolden(tester, 'my_app_single_scenario');
   });
 
-    testGoldens('DeviceBuilder - one scenario - override devices',
+  testGoldens('DeviceBuilder - one scenario - override devices',
       (tester) async {
     final builder = DeviceBuilder()
       ..overrideDevicesForAllScenarios(devices: [
@@ -37,5 +35,53 @@ void main() {
 
     await screenMatchesGolden(
         tester, 'my_app_page_single_scenario_more_devices');
+  });
+
+  testGoldens('DeviceBuilder - multiple scenarios - with onCreate',
+      (tester) async {
+    final builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: [
+        Device.phone,
+        Device.iphone11,
+        Device.tabletPortrait,
+        Device.tabletLandscape,
+      ])
+      ..addScenario(
+        widget: MyApp(),
+        name: 'default page',
+      )
+      ..addScenario(
+        widget: MyApp(),
+        name: 'tap once',
+        onCreate: (scenarioWidgetKey) async {
+          final finder = find.descendant(
+            of: find.byKey(scenarioWidgetKey),
+            matching: find.byIcon(Icons.add),
+          );
+          expect(finder, findsOneWidget);
+          await tester.tap(finder);
+        },
+      )
+      ..addScenario(
+        widget: MyApp(),
+        name: 'tap five times',
+        onCreate: (scenarioWidgetKey) async {
+          final finder = find.descendant(
+            of: find.byKey(scenarioWidgetKey),
+            matching: find.byIcon(Icons.add),
+          );
+          expect(finder, findsOneWidget);
+
+          await tester.tap(finder);
+          await tester.tap(finder);
+          await tester.tap(finder);
+          await tester.tap(finder);
+          await tester.tap(finder);
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+
+    await screenMatchesGolden(tester, 'my_app_page_multiple_scenarios');
   });
 }
