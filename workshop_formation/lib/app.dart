504e67bbe4fe388373a2b1751a8ac0285af36310
bwnyasse <risbonyasse@gmail.com>
2020-12-08 17:57:48 -0500

    codelabs(navigation2.0): router_delegate

--- workshop_formation/lib/app.dart
+++ workshop_formation/lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:workshop_formation/models/models.dart';
+import 'package:workshop_formation/routes/route_delegate.dart';
+import 'package:workshop_formation/routes/route_parser.dart';
 import 'package:workshop_formation/screens/screen_1.dart';
 import 'package:workshop_formation/screens/screen_2.dart';
 import 'package:workshop_formation/screens/screen_404.dart';
@@ -10,52 +12,15 @@ class BooksApp extends StatefulWidget {
 }
 
 class _BooksAppState extends State<BooksApp> {
-  Book _selectedBook;
-  bool show404 = false;
-  final List<Book> books = availableBooks();
-
-  void _handleBookTapped(Book book) {
-    setState(() {
-      _selectedBook = book;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    BookRouterDelegate _routerDelegate = BookRouterDelegate();
+    BookRouteInformationParser _routeInformationParser =
+        BookRouteInformationParser();
+    return MaterialApp.router(
       title: 'Books App - Navigation 2.0',
-      home: Navigator(
-        //RESOLUTION: Fournit la liste des pages au Navigator en renseignant l'attribut pages
-        pages: [
-          MaterialPage(
-            key: ValueKey('Screen1'),
-            child: Screen1(
-              books: books,
-              onTapped: _handleBookTapped,
-            ),
-          ),
-          if (show404)
-            MaterialPage(key: ValueKey('UnknownPage'), child: UnknownScreen())
-          else if (_selectedBook != null)
-            MaterialPage(
-                key: ValueKey('Screen2$_selectedBook'),
-                child: Screen2(book: _selectedBook))
-        ],
-
-        //RESOLUTION : Comment faire le pop des pages ?
-        onPopPage: (route, result) {
-          if (!route.didPop(result)) {
-            return false;
-          }
-
-          // Update the list of pages by setting _selectedBook to null
-          setState(() {
-            _selectedBook = null;
-          });
-
-          return true;
-        },
-      ),
+      routerDelegate: _routerDelegate,
+      routeInformationParser: _routeInformationParser,
     );
   }
 }
--- workshop_formation/lib/routes/route_delegate.dart
+++ workshop_formation/lib/routes/route_delegate.dart
@@ -1,21 +1,84 @@
 import 'package:flutter/material.dart';
 import 'package:workshop_formation/models/models.dart';
+import 'package:workshop_formation/screens/screen_1.dart';
+import 'package:workshop_formation/screens/screen_2.dart';
+import 'package:workshop_formation/screens/screen_404.dart';
 
 class BookRouterDelegate extends RouterDelegate<BookRoutePath>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin<BookRoutePath> {
+  Book _selectedBook;
+  bool show404 = false;
+  List<Book> books = availableBooks();
+
+  BookRoutePath get currentConfiguration {
+    if (show404) {
+      return BookRoutePath.unknown();
+    }
+    return _selectedBook == null
+        ? BookRoutePath.home()
+        : BookRoutePath.details(books.indexOf(_selectedBook));
+  }
+
+  void _handleBookTapped(Book book) {
+    _selectedBook = book;
+    notifyListeners();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO-3
-    throw UnimplementedError();
+    return Navigator(
+      pages: [
+        MaterialPage(
+          key: ValueKey('Screen1'),
+          child: Screen1(
+            books: books,
+            onTapped: _handleBookTapped,
+          ),
+        ),
+        if (show404)
+          MaterialPage(key: ValueKey('UnknownPage'), child: UnknownScreen())
+        else if (_selectedBook != null)
+          MaterialPage(
+              key: ValueKey('Screen2$_selectedBook'),
+              child: Screen2(book: _selectedBook))
+      ],
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) {
+          return false;
+        }
+
+        // Update the list of pages by setting _selectedBook to null
+        _selectedBook = null;
+        show404 = false;
+        notifyListeners();
+
+        return true;
+      },
+    );
   }
 
   @override
-  // TODO-4
-  GlobalKey<NavigatorState> get navigatorKey => throw UnimplementedError();
+  GlobalKey<NavigatorState> get navigatorKey => GlobalKey<NavigatorState>();
 
   @override
-  Future<void> setNewRoutePath(BookRoutePath configuration) {
-    // TODO-5
-    throw UnimplementedError();
+  Future<void> setNewRoutePath(BookRoutePath configuration) async {
+    if (configuration.isUnknown) {
+      _selectedBook = null;
+      show404 = true;
+      return;
+    }
+
+    if (configuration.isDetailsPage) {
+      if (configuration.id < 0 || configuration.id > books.length - 1) {
+        show404 = true;
+        return;
+      }
+
+      _selectedBook = books[configuration.id];
+    } else {
+      _selectedBook = null;
+    }
+
+    show404 = false;
   }
-} 
+}
