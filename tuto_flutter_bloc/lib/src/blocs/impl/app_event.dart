5fb0dec78ec1c8b6605d3f1f0dd1c24fec8875cc
bwnyasse <risbonyasse@gmail.com>
2020-12-18 00:55:05 +0100

    tuto-flutter-bloc 3

---
+++ tuto_flutter_bloc/lib/src/blocs/impl/app_event.dart
@@ -0,0 +1,15 @@
+abstract class AppEvent {}
+
+class LoadEvent extends AppEvent {}
+
+///
+/// Juste pour le tuto
+/// Dans la réalité vous aurez peut être besoin de le créer
+///
+class ErrorEvent extends AppEvent {}
+
+///
+/// Juste pour le tuto
+/// Dans la réalité vous aurez peut être besoin de le créer
+///
+class LoadingEvent extends AppEvent {}
