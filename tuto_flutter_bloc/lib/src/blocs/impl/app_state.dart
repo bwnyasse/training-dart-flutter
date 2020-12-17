2467b570922ddb9e815b1b30687fdad2456df093
bwnyasse <risbonyasse@gmail.com>
2020-12-18 00:52:38 +0100

    tuto-flutter-bloc 2

---
+++ tuto_flutter_bloc/lib/src/blocs/impl/app_state.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tuto_flutter_bloc/src/models/models.dart';
+
+abstract class AppState {}
+
+class AppError extends AppState {}
+
+class AppEmpty extends AppState {}
+
+class AppLoading extends AppState {}
+
+class AppLoaded extends AppState {
+  final MoviesResponse response;
+
+  AppLoaded({@required this.response});
+}
