beb3e8b47a6c3ac95c214c9187c4bbc7f28b2ca7
bwnyasse <risbonyasse@gmail.com>
2020-12-18 01:03:35 +0100

    tuto-flutter-bloc 4

---
+++ tuto_flutter_bloc/lib/src/blocs/impl/app_bloc.dart
@@ -0,0 +1,45 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:tuto_flutter_bloc/src/blocs/blocs.dart';
+import 'package:tuto_flutter_bloc/src/services/services.dart';
+
+class AppBloc extends Bloc<AppEvent, AppState> {
+  final ApiService service;
+  final AppState initWithState;
+
+  AppBloc({
+    @required this.service,
+    this.initWithState,
+  }) : super(AppEmpty());
+
+  @override
+  Stream<AppState> mapEventToState(AppEvent event) async* {
+    //
+    // ON ERROR EVENT
+    //
+    if (event is ErrorEvent) {
+      yield AppError();
+    }
+
+    //
+    // ON LOADING EVENT
+    //
+    if (event is LoadingEvent) {
+      yield AppLoading();
+    }
+
+    //
+    // ON LOAD EVENT
+    //
+    if (event is LoadEvent) {
+      yield AppLoading();
+      try {
+        final list = await service.loadMovies();
+        yield AppLoaded(response: list);
+      } catch (e) {
+        print(e);
+        yield AppError();
+      }
+    }
+  }
+}
