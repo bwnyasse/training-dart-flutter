310fb2d92076d9f09a2d5f5eb5ba9a2a34494feb
bwnyasse <risbonyasse@gmail.com>
2021-01-02 14:35:08 +0100

    sample(dart-cli): options and update README.md

--- sample_dart_cli/README.md
+++ sample_dart_cli/README.md
@@ -1,5 +1,40 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+# training-dart-flutter
 
-Created from templates made available by Stagehand under a BSD-style
-[license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
+## sample dart cli
+
+L'objectif est de rapidement comprendre l'utilisation de `dart` pour construire une application `cli` : 
+
+- entrypoint dans le `bin/`
+- le code dans le `lib/`
+- éventuellement des tests dans `test/`
+
+### Fonctionnalité : 
+
+- On utilise l'api [openweathermap](https://openweathermap.org/api) pour récupérer le climat.
+- On essaie de comprendre l'utilisation de 
+    
+        $ dart2native
+
+        $ dartaotruntime
+
+- Manipulation de `part` , `part of`, sérialisation/désérialisation avec `json_annotation`
+
+### Manipulation 
+
+Ci-dessous , des actions possibles pour tester le code implémenté : 
+
+        $ dart bin/main.dart 
+
+        $ dart bin/main.dart -z 75000
+
+        $ dart2native bin/main.dart -o weathercli
+
+        $ ./weathercli 
+
+        $ ./weathercli -z 75000
+
+        $ dart2native bin/main.dart -k aot
+
+        $ dartaotruntime bin/main.aot
+
+        $ dartaotruntime bin/main.aot -z 75000
--- sample_dart_cli/bin/main.dart
+++ sample_dart_cli/bin/main.dart
@@ -1,24 +1,53 @@
 import 'dart:io';
-import 'package:args/args.dart' show ArgParser;
+import 'package:args/args.dart' show ArgParser, ArgResults;
 import 'package:sample_dart_cli/weather.dart' as weather;
 
-void main(List<String> arguments) {
-  /*
-  *  0 = Success
-  *  1 = Warnings
-  *  2 = Errors
-  */
-  exitCode = 0;
+ArgParser _configureArgParser() => ArgParser()
+  ..addOption(
+    'zip',
+    help: '[required] The zip code',
+    abbr: 'z',
+  )
+  ..addOption(
+    'country',
+    help: 'The country code',
+    abbr: 'c',
+    defaultsTo: 'fr',
+  )
+  ..addFlag(
+    'help',
+    abbr: 'h',
+    help: 'Show the usage',
+  );
 
-  final parser = ArgParser()
-    ..addOption('zip', abbr: 'z')
-    ..addOption('country', abbr: 'c', defaultsTo: 'fr');
+int _checkArgResults(List<String> args, ArgParser parser, ArgResults results) {
+  if (results['help'] == true || args.isEmpty) {
+    print(parser.usage);
+    return -1;
+  }
 
-  final argResults = parser.parse(arguments);
+  // Check zip as required
+  if (results['zip'] == null) {
+    print(parser.usage);
+    return -1;
+  }
+
+  return 0;
+}
+
+void main(List<String> args) {
+  final parser = _configureArgParser();
+
+  final argResults = parser.parse(args);
+  final code = _checkArgResults(args, parser, argResults);
+  if (code != 0) {
+    exit(code);
+  }
+
   final zip = argResults['zip'];
   final country = argResults['country'];
   if (country == 'fr') {
-    assert(zip.length > 4, 'For france , the zip must have 5 digits');
+    assert(zip.length > 4, 'For france, the zip must have 5 digits');
   }
   weather.execute(zip, country);
 }
