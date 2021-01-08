3f83164358d1d0f4059a6d0391069f76cc8fd10d
bwnyasse <risbonyasse@gmail.com>
2021-01-08 23:47:00 +0100

    responsive(mediaquery): resolved

--- workshop_006_responsive_layout/lib/layout/media_query_layout.dart
+++ workshop_006_responsive_layout/lib/layout/media_query_layout.dart
@@ -5,7 +5,21 @@ import 'package:workshop_006_responsive_layout/utils/my_colors.dart'
 class MediaQueryLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //TODO: Use MediaQuery
-    return Center(child: Text("TODO use MediaQuery"));
+    Size screenSize = MediaQuery.of(context).size;
+    Orientation orientation = MediaQuery.of(context).orientation;
+
+    return Scaffold(
+      body: Container(
+        color: my_colors.darkGreen,
+        child: Center(
+          child: Text(
+            '''View with MediaQuery
+          [width]: ${screenSize.width.toStringAsFixed(2)}
+          [orientation]: $orientation''',
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+        ),
+      ),
+    );
   }
 }
