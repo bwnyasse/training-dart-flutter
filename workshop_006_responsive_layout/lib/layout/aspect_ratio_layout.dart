590b7878396ce022c85d651599b505743a28aa62
bwnyasse <risbonyasse@gmail.com>
2021-01-09 00:27:47 +0100

    responsive(AspectRatio): resolved

--- workshop_006_responsive_layout/lib/layout/aspect_ratio_layout.dart
+++ workshop_006_responsive_layout/lib/layout/aspect_ratio_layout.dart
@@ -6,7 +6,44 @@ import 'package:workshop_006_responsive_layout/utils/my_colors.dart'
 class AspectRatioLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //TODO: Use AspectRatioAspectRatio
-    return Center(child: Text("TODO Use AspectRatio"));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            AspectRatioWidget(ratio: '16 / 9'),
+            AspectRatioWidget(ratio: '3 / 2'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AspectRatioWidget extends StatelessWidget {
+  final String ratio;
+
+  AspectRatioWidget({@required this.ratio});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: Fraction.fromString(ratio).toDouble(),
+      child: Container(
+        decoration: BoxDecoration(
+          color: my_colors.darkGreen,
+          border: Border.all(color: Colors.white),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Text(
+              'AspectRatio - $ratio',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
