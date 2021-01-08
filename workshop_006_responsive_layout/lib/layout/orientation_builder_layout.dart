f61baa7b9f4a1ff71f89375bdc1e1e9c0bd2c8a6
bwnyasse <risbonyasse@gmail.com>
2021-01-09 00:09:29 +0100

    responsive(OrientationBuilder): resolved

--- workshop_006_responsive_layout/lib/layout/orientation_builder_layout.dart
+++ workshop_006_responsive_layout/lib/layout/orientation_builder_layout.dart
@@ -5,8 +5,45 @@ import 'package:workshop_006_responsive_layout/utils/my_colors.dart'
 class OrientationBuilderLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //TODO: Use OrientationBuilder
-    return Center(child: Text('TODO use OrientationBuilder'));
+    Orientation deviceOrientation = MediaQuery.of(context).orientation;
 
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: my_colors.darkGreen,
+              child: OrientationBuilder(
+                builder: (context, orientation) => Center(
+                  child: Text(
+                    'View 1\n\n' +
+                        '[MediaQuery orientation]:\n$deviceOrientation\n\n' +
+                        '[OrientationBuilder]:\n$orientation',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: OrientationBuilder(
+              builder: (context, orientation) => Container(
+                color: Colors.white,
+                child: Center(
+                  child: Text(
+                    'View 2\n\n' +
+                        '[MediaQuery orientation]:\n$deviceOrientation\n\n' +
+                        '[OrientationBuilder]:\n$orientation',
+                    style: TextStyle(color: my_colors.darkGreen, fontSize: 18),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
