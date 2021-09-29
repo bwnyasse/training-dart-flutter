dadc582ce31e3c371e66e87d283d5665893cb778
bwnyasse <risbonyasse@gmail.com>
2021-09-29 15:02:16 -0400

    tuto-flutter-bloc-0 null safety

--- tuto_flutter_bloc/lib/src/widgets/impl/home.dart
+++ tuto_flutter_bloc/lib/src/widgets/impl/home.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:tuto_flutter_bloc/src/models/models.dart';
 import 'package:tuto_flutter_bloc/src/services/impl/api_service.dart';
 import 'package:tuto_flutter_bloc/src/widgets/widgets.dart';
 import 'package:provider/provider.dart';
 
 class MyHomePage extends StatefulWidget {
   final String title;
-  MyHomePage({@required this.title});
+  MyHomePage({required this.title});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -73,7 +72,7 @@ class ErrorWidget extends StatelessWidget {
 class LoadingWidget extends StatelessWidget {
   final String message;
 
-  LoadingWidget({@required this.message});
+  LoadingWidget({required this.message});
   @override
   Widget build(BuildContext context) {
     return Center(
