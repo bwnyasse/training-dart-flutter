a50596f84bc52f903e50133e496c10ea7ba78f4c
bwnyasse <risbonyasse@gmail.com>
2021-10-05 16:02:47 -0400

    sample dart & java grpc step 0

--- sample_009_grpc/client/bin/main.dart
+++ sample_009_grpc/client/bin/main.dart
@@ -1,29 +1,3 @@
-import 'package:grpc/grpc.dart';
-import 'package:sample_dart_grpc_client/generated/server/src/main/proto/movie-service.pbgrpc.dart';
-
 Future<void> main(List<String> args) async {
-  final channel = ClientChannel(
-    'localhost',
-    port: 9090,
-    options: ChannelOptions(
-      credentials: ChannelCredentials.insecure(),
-    ),
-  );
-  final stub = MovieCatalogClient(channel);
-
-  //Create a movie
-  MovieItem terminator2 = MovieItem()
-    ..name = 'Terminator 2'
-    ..price = 20.95
-    ..inStock = true;
-
-  print('Saving movie:  $terminator2');
-  final addMovieResponse = await stub.saveNewMovie(terminator2);
-  print('Response is => :  $addMovieResponse');
-
-  final fetchMovieRequest = FetchMovieRequest()..name = 'The Lion King';
-  print('Fetching movie:  $fetchMovieRequest');
-  final movieItem = await stub.fetchExistingMovie(fetchMovieRequest);
-  print('Response is => :  $movieItem');
-  await channel.shutdown();
+  print("sample grpc");
 }
