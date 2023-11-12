7cc6ec4c80ecc1564ca8b88487cdfc05c661da56
bwnyasse <5323628+bwnyasse@users.noreply.github.com>
2023-11-12 17:35:23 -0500

    upgrade dart 3

--- workshop_formation/android/app/src/main/kotlin/net/bwnyasse/workshop_formation/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package net.bwnyasse.workshop_formation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- workshop_formation/lib/src/screens/screens.dart
+++ workshop_formation/lib/src/screens/screens.dart
@@ -38,13 +38,13 @@ class _Screen1State extends State<Screen1> {
     super.initState();
     _loadMovies();
   }
-
+ 
   _loadMovies() async {
     // RESOLUTION : Charger les films à afficher depuis l'API MoviesDB
     final apiService = context.read<ApiService>();
     MoviesResponse response = await apiService.loadMovies();
     setState(() {
-      movies = response.movies;
+      movies = response.movies!;
     });
   }
 
@@ -53,7 +53,7 @@ class _Screen1State extends State<Screen1> {
     AppLocalizations localizations = AppLocalizations.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text(localizations.getValue(LocaleKey.screen1Title)),
+        title: Text(localizations.getValue(LocaleKey.screen1Title)!),
       ),
       //RESOLUTION: Remplacer le body par une liste simple en conservant la navigation
       body: ListView.builder(
@@ -61,8 +61,8 @@ class _Screen1State extends State<Screen1> {
         itemCount: movies.length,
         itemBuilder: (BuildContext context, int index) {
           Movie movie = movies[index];
-          String originalTitle = movie.originalTitle;
-          String releaseDate = movie.releaseDate;
+          String originalTitle = movie.originalTitle!;
+          String releaseDate = movie.releaseDate!;
           return ListItem(
             data: "$originalTitle - Date : $releaseDate",
             onTapCallback: () {
@@ -87,10 +87,10 @@ class Screen2 extends StatelessWidget {
   Widget build(BuildContext context) {
     AppLocalizations localizations = AppLocalizations.of(context);
     //RESOLUTION: Comment récuperer l'argument dans l'écran ?
-    final ScreenArguments args = ModalRoute.of(context).settings.arguments;
+    final ScreenArguments args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
     return Scaffold(
       appBar: AppBar(
-        title: Text(localizations.getValue(LocaleKey.screen2Title)),
+        title: Text(localizations.getValue(LocaleKey.screen2Title)!),
       ),
       body: Center(
         child: Column(
